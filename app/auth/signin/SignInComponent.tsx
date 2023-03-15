3f82a4004c5e37e75ef5035dd18b4fdeb72cfb4f
Abdul <awsayed@alaska.edu>
2023-03-14 19:03:11 -0700

    fix type error in signin component

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import { getProviders } from "next-auth/react";
+import { BuiltInProviderType } from "next-auth/providers";
+import { ClientSafeProvider, getProviders, LiteralUnion } from "next-auth/react";
 import { signIn } from "next-auth/react";
 
+// type Props = {
+//   providers: Awaited<ReturnType<typeof getProviders>>;
+// };
 type Props = {
-  providers: Awaited<ReturnType<typeof getProviders>>;
+  providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null;
 };
 
 export default function SignInComponent({ providers }: Props) {
   return (
     <div className="flex justify-center">
-      {Object.values(providers!).map((provider) => (
+      {Object.values(providers || {}).map((provider) => (
         <div key={provider.name}>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
