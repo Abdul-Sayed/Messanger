1818a2192469db670010803b6e520485e4da92e4
Abdul <awsayed@alaska.edu>
2023-06-20 19:07:14 -0700

    add github OAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
+import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     FacebookProvider({
       clientId: process.env.Facebook_ID!,
       clientSecret: process.env.Facebook_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.Github_ID!,
+      clientSecret: process.env.Github_SECRET!,
     }),
   ],
 
